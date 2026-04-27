1f91e25fb128a87d83421e062cffd0bfd4dd7a8e
terry-u16 <terry-u16@users.noreply.github.com>
2026-04-28 00:03:04 +0900

    chore: resolve dependency update warnings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  build: {
+    chunkSizeWarningLimit: 1024,
+  },
 });
